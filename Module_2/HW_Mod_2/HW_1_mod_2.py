1b4e2c9a51efdb8718b4271acc6de23948155269
IhorKomkov <ihorkomkov@gmail.com>
2021-09-14 22:51:23 +0300

    v 0.0.2 with second, but have some issuse

--- Module_2/HW_Mod_2/HW_1_mod_2.py
+++ Module_2/HW_Mod_2/HW_1_mod_2.py
@@ -5,37 +5,36 @@
 # Напишите классы сериализации контейнеров с данными Python в json, bin файлы.
 # Сами классы должны соответствовать общему интерфейсу (абстрактному базовому
 # классу) SerializationInterface.
-from abc import ABC, abstractmethod
-import json
-import pickle
+# from abc import ABC, abstractmethod
+# import json
+# import pickle
+#
+#
+# class SerializationInterface(ABC):
+#     @abstractmethod
+#     def serialise_this(self):
+#         pass
+#
+#
+# class SerializationInBin(SerializationInterface):
+#     """serialise in bin"""
+#     def __init__(self, data):
+#         self.data = data
+#
+#     def serialise_this(self):
+#         return pickle.dump(self.data)
+#
+#
+# class SerializationInJson(SerializationInterface):
+#     """serialise in json"""
+#     def __init__(self, data):
+#         self.data = data
+#
+#     def serialise_this(self):
+#         return json.dump(self.data)
 
 
-class SerializationInterface(ABC):
-    @abstractmethod
-    def serialise_this(self):
-        pass
 
-
-class SerializationInBin(SerializationInterface):
-    """serialise in bin"""
-    def __init__(self, data: object):
-        self.data = data
-
-    def serialise_this(self):
-        return pickle.dumps(self.data)
-
-
-class SerializationInJson(SerializationInterface):
-    """serialise in json"""
-    def __init__(self, data: object):
-        self.data = data
-
-    def serialise_this(self):
-        return json.dumps(self.data)
-
-
-
-json_serialise = SerializationInJson()
 
 
 # ex #2
@@ -50,9 +49,10 @@ json_serialise = SerializationInJson()
 
 
 class Meta(type):
-    pass
-
- # тут должно быть ваше решение
+    def __new__(cls, name_class, base, atrrib):
+        cls.atrrib = {'class_number': cls.children_number}
+        cls.children_number += 1
+        return type(name_class, base, cls.atrrib)
 
 
 Meta.children_number = 0
@@ -63,14 +63,10 @@ class Cls1(metaclass=Meta):
         self.data = data
 
 
-
-
-
 class Cls2(metaclass=Meta):
     def __init__(self, data):
         self.data = data
 
-
-# assert (Cls1.class_number, Cls2.class_number) == (0, 1)
-# a, b = Cls1(''), Cls2('')
-# assert (a.class_number, b.class_number) == (0, 1)
+assert (Cls1.class_number, Cls2.class_number) == (0, 1)
+a, b= Cls1(''), Cls2('')
+assert (a.class_number, b.class_number) == (0, 1)
